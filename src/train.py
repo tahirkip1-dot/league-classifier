90925fd02572bd6087f026d23706c573dabba5d5
tahir kip <tahir.kip1@gmail.com>
2026-08-11 14:41:56 +1000

    move evaluate into train

--- src/train.py
+++ src/train.py
@@ -7,7 +7,6 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, random_split
 
-from evaluate import evaluate
 from model import (
     NUM_CHAMPIONS_PER_GAME,
     LeagueDraftModel,
@@ -58,6 +57,29 @@ class ChampionDataset(Dataset):
         masked_match[champ_id] = self.masked_id
         
         return masked_match, masked_champ
+
+def evaluate(model, loader, loss_fn, device):
+
+    model.eval()
+
+    running_loss = 0.0
+    num_examples = 0
+
+    with torch.inference_mode():
+        for x_v, y_v in loader:
+
+            batch_size = y_v.size(0)
+            
+            x_v = x_v.to(device, non_blocking=(device.type == 'cuda'))
+            y_v = y_v.to(device, non_blocking=(device.type == 'cuda'))
+
+            logits = model(x_v)
+            loss = loss_fn(logits, y_v)
+
+            running_loss += loss.item() * batch_size
+            num_examples += batch_size
+
+    return running_loss / num_examples
 
 
 def train_epoch(model, loader, optimizer, loss_fn, device):
