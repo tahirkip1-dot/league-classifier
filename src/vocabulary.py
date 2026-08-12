e88893c8ee945f93af2bedf776b55cb1bade7621
tahir kip <tahir.kip1@gmail.com>
2026-08-12 16:36:23 +1000

    create vocabulary object

---
+++ src/vocabulary.py
@@ -0,0 +1,11 @@
+class Vocabulary:
+
+    def __init__(self, champ_names: list):
+        self.names = [name.lower() for name in champ_names].append('masked')
+
+    def encode(self, name: str):
+        name = name.lower()
+        return self.names.index(name)
+
+    def decode(self, id: int):
+        return self.names[id]
