db8ba7080fa1d8f3fc41317c3113f9bbec50c6a8
tahir kip <tahir.kip1@gmail.com>
2026-08-27 16:38:16 +1000

    continue adding ban masking

--- src/vocabulary.py
+++ src/vocabulary.py
@@ -11,10 +11,23 @@ class Vocabulary:
         names = list(riot_id_name.values()) + ['masked']
         self.name_id = dict(zip(names, range(len(names))))
 
+        mask_id = self.name_id['masked']
+        self.mask_id = mask_id
+
+        # model integer to name dictionary
+        self.id_name = {value:key for key, value in self.name_id.items()}
+
 
     def riot_id_to_id(self, riot_id: int):
         name = self.riot_id_name[riot_id]
         return self.name_id[name]
+
+    def name_to_id(self, name: str):
+        return self.name_id[name]
+
+
+    def id_to_name(self, id: int):
+        return self.id_name[id]
 
     def __len__(self):
         return len(self.name_id.items())
