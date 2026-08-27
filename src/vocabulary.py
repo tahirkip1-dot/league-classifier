e948e326e7f7f521925ec7aff59df6aae7a3c1f4
tahir kip <tahir.kip1@gmail.com>
2026-08-27 16:12:15 +1000

    change vocab object to manage all mappings

--- src/vocabulary.py
+++ src/vocabulary.py
@@ -1,16 +1,29 @@
 class Vocabulary:
-    '''adds the masked champion, dont add manually beforehand'''
-    def __init__(self, champ_names: list[str]):
-        self.names = [name.casefold() for name in champ_names] + ['masked'.casefold()]
+    '''takes riot id to names mapping and builds maps to and from names, riot_ids, model integers'''
+    def __init__(self, riot_id_to_name: dict[int, str]):
 
-    def encode(self, name: str):
-        return self.names.index(name.casefold())
+        # riot id to name dictionary
+        riot_id_name = riot_id_to_name
+        riot_id_name[-1] = 'no_ban'
+        self.riot_id_name = riot_id_name
 
+        # name to model integer dictionary
+        names = list(riot_id_name.values()) + ['masked']
+        self.name_id = dict(zip(names, range(len(names))))
+
+
+    def riot_id_to_id(self, riot_id: int):
+        name = self.riot_id_name[riot_id]
+        return self.name_id[name]
+
+    def __len__(self):
+        return len(self.name_id.items())
+    
+'''
     def decode(self, id: int):
         return self.names[id]
 
     def mask_id(self):
         return self.encode('masked')
-
-    def __len__(self):
-        return len(self.names)
+'''
+    
